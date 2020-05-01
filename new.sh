868ad7c0b9b5bd72431e803795b6fb181d19111a
Rabindra <rabindhakal.aipa@gmail.com>
2020-05-01 04:56:21 -0400

    adding birth place

--- new.sh
+++ new.sh
@@ -5,9 +5,11 @@ read -p 'name:' my_name
 read -p 'age:' my_age 
 read -p 'sex:' my_sex 
 read -p 'dob:' my_dob 
+read -p 'dob:' my_birth_place 
 echo "my name is $my_name"
 echo "my name is $my_age"
 echo "my name is $my_sex"
 echo "my name is $my_dob"
-echo "my name is $my_name i am $my_age years $sex and My dob is $my_dob"
+echo "my name is $my_birth_place"
+echo "my name is $my_name i am $my_age years $sex and My dob is $my_dob at $my_birth_place"
 
