89c72cb94a2f4abe8cce10ea9fd5005ff699e82a
Rabindra <rabindhakal.aipa@gmail.com>
2020-05-01 04:27:31 -0400

    adding bash script which will print details afer user input

---
+++ new.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# getting user output for name and birth place
+
+read -p 'name:' my_name 
+read -p 'age:' my_age 
+read -p 'sex:' my_sex 
+read -p 'dob:' my_dob 
+echo "my name is $my_name"
+echo "my name is $my_age"
+echo "my name is $my_sex"
+echo "my name is $my_dob"
+echo "my name is $my_name i am $my_age years $sex and My dob is $my_dob"
+
