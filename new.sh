3d12d51a10817efe5a86d95886a7ecbc23490435
Rabindra <rabindhakal.aipa@gmail.com>
2020-05-02 04:36:44 -0400

    testing done

--- new.sh
+++ new.sh
@@ -12,4 +12,4 @@ echo "my name is $my_sex"
 echo "my name is $my_dob"
 echo "my name is $my_birth_placett"
 echo "my name is $my_name i am $my_age years $sex and My dob is $my_dob at $my_birth_place"
-
+# Testing is done!!!
