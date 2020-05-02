dd2fbb02c4baa8c7e410ca71c6faced36cc61552
Rabindra <rabindhakal.aipa@gmail.com>
2020-05-02 03:25:21 -0400

    :

--- new.sh
+++ new.sh
@@ -10,6 +10,6 @@ echo "my name is $my_name"
 echo "my name is $my_age"
 echo "my name is $my_sex"
 echo "my name is $my_dob"
-echo "my name is $my_birth_place"
+echo "my name is $my_birth_placett"
 echo "my name is $my_name i am $my_age years $sex and My dob is $my_dob at $my_birth_place"
 
