73b3d831ad2ced59a446384ee82b5cc2836e48b6
Rabindra <rabindhakal.aipa@gmail.com>
2020-05-02 04:13:25 -0400

    Fixing calculation code

--- calculate.sh
+++ calculate.sh
@@ -1,9 +1,7 @@
-ls
-just trying to learn command for calculating two numbers
 #!/bin/bash
-
 # calculate two numbers
 
+#just trying to learn command for calculating two numbers
 read -p 'first_number:' number_one
 read -p 'second_number:' number_two
 ans=$((number_one*number_two))
